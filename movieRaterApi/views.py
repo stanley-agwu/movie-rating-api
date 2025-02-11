a210ca980e024d2f5383169adf01aadb7b803569
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2025-02-11 20:36:21 +0100

    feat(rating): custom movie rating method

--- movieRaterApi/views.py
+++ movieRaterApi/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
+from django.contrib.auth.models import User
+
 from movieRaterApi.models import Movie, Rating
 from movieRaterApi.serializers import MovieSerializer, RatingSerializer
 
@@ -7,6 +12,32 @@ from movieRaterApi.serializers import MovieSerializer, RatingSerializer
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+    @action(detail=True, methods=['POST'])
+    def rate_movie(self, request, pk=None):
+        if 'rating' in request.data:
+            movie = Movie.objects.get(id=pk)
+            rating = request.data['rating']
+            # user = request.user
+            user = User.objects.get(id=1)
+
+            try:
+                movie_rating = Rating.objects.get(user=user.id, movie=movie.id)
+                movie_rating.rating = rating
+                movie_rating.save()
+                serializer = RatingSerializer(rating, many=False)
+                response = {"message": "movie rating updated successfully", "result": serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+            except:
+                rating = Rating.objects.create(user=user, movie=movie, rating=rating)
+                serializer = RatingSerializer(rating, many=False)
+                response = {"message": "movie rating created successfully", "result": serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+        else:
+            response = {"message": "rating not provided in request"}
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+
 
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
